84703c2e769271a09abd0e33277b53d8fe929ecb
Guilherme <guidanirs@gmail.com>
2020-12-07 11:17:03 -0300

    Adding database access

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return (SellerDao) new SellerDaoJDBC(DB.getConnection());
+		}
+	public static DepartmentDao createDepartmentDao() {
+		return (DepartmentDao) new DepartmentDaoJDBC(DB.getConnection());
+	}
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert (Seller obj);
+	void uptade (Seller obj);
+	void deleteById (Integer Id);
+	Seller findById (Integer Id);
+	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
+}
